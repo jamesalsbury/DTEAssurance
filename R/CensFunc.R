c2620219e9c6a90541aba5816e2d4b9cd9f162e6
James Salsbury <jsalsbury1@sheffield.ac.uk>
2024-03-14 14:51:42 +0000

    adding to the R package

--- R/CensFunc.R
+++ R/CensFunc.R
@@ -1,13 +1,25 @@
 #' Function to censor a data set
 #'
-#' @param dataCombined a data frame
-#' @param censTime Time at whcih you wish to perform the censoring
-#' @return A dataframe
+#' @param dataCombined An uncensored dataframe
+#' @param censEvents Number of events at which you wish to perform the censoring
+#' @param censTime Time at which you wish to perform the censoring
+#' @return A censored dataframe
 #' @export
 
 
-CensFunc <- function(dataCombined, censTime){
-  dataCombined$status <- dataCombined$pseudoTime < censTime
+CensFunc <- function(dataCombined, censEvents = NULL, censTime = NULL){
+
+  if (is.null(censEvents) && is.null(censTime)) {
+    stop("Either censEvents or censTime must be specified")
+  }
+
+  if (!is.null(censEvents)){
+    dataCombined <- dataCombined[order(dataCombined$pseudoTime),]
+    censTime <- dataCombined$pseudoTime[censEvents]
+  }
+
+
+  dataCombined$status <- dataCombined$pseudoTime <= censTime
   dataCombined$status <- dataCombined$status * 1
   dataCombined$enrolled <- dataCombined$recTime < censTime
   dataCombined <- dataCombined[dataCombined$enrolled, ]
