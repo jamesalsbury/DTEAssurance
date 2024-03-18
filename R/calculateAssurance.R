a4e123f947f21453f4ba09bec97a1025ab0df959
James Salsbury <jsalsbury1@sheffield.ac.uk>
2024-03-18 15:01:09 +0000

    adding to the package

--- R/calculateAssurance.R
+++ R/calculateAssurance.R
@@ -1,45 +1,64 @@
-calculateAssurance <- function(n_C, n_E, lambda_C, gammac, P_E, P_DTE, delayT, HRStar, nEvents,
-                               rec_method, analysis_method, rho = 0, gamma = 0, nSims=1e5){
+#' Function to simulate a data set, in the context of delayed treatment effects
+#'
+#' @param n_C Sample size in control group
+#' @param n_E Sample size in experimental treatment group
+#' @param lambda_C Weibull control parameter
+#' @param HRSTar The post-delay hazard ratio
+#' @param HRStarDist The chosen parametric distribution for the post-delay hazard ratio
+#' @param gamma_C Weibull control parameter
+#' @param gamma_E Weibull experimental treatment parameter
+#' @param delayT Length of delay
+#' @param delayTDist The chosen parametric distribution for the length of delay
+#' @param P_S Probability of the Kaplan-Meier curves separating at some time
+#' @param P_DTE Probability of the treatment being subject to a delay (given the K-M curves will separate)
+#' @param nEvents Number of events to be censored at
+#' @param censTime Time of censoring
+#' @param rec_method Recruitment method
+#' @param rec_period Recruitment period (for the power method)
+#' @param rec_power Recruitment power (for the power method)
+#' @param rec_rate Recruitment rate (for the piecewise constant method)
+#' @param rec_duration Recruitment duration (for the piecewise constant method)
+#' @param analysis_method Method of analysis (log-rank test or weighted log-rank test)
+#' @param rho Rho parameter for the Fleming-Harrington weighted log-rank test
+#' @param gamma Gamma parameter for the Fleming-Harrington weighted log-rank test
+#'
+#' @return A value
+#' @export
 
-  control_n <- length(lambdac)
+calculateAssurance <- function(n_C, n_E, lambda_C, HRStar, HRStarDist = "hist", gamma_C, gamma_E, delayT, delayTDist = "hist",
+                               P_S = 1, P_DTE = 0, censEvents=NULL, censTime = NULL, rec_method, rec_period=NULL, rec_power=NULL, rec_rate=NULL, rec_duration=NULL,
+                               analysis_method, rho = 0, gamma = 0, nSims=1e5){
+
+  control_n <- length(lambda_C)
+
+  assVec <- rep(NA, nSims)
 
   for (i in 1:nSims){
 
     #Sample the control parameters
     u <- sample(1:control_n, size = 1)
-    sampled_lambdac <- lambdac[u]
-    sampled_gammac <- gammac[u]
+    sampled_lambdac <- lambda_C[u]
+    sampled_gammac <- gamma_C[u]
 
     #Sample the treatment effect parameters
-    u <- sample(1:delayT, size = 1)
-    sampled_delayT <- delayT[u]
-    sampled_HRStar <- HRStar[u]
+    sampled_HRStar <- SHELF::sampleFit(HRStar, n = 1)[,HRStarDist]
+    sampled_delayT <- SHELF::sampleFit(delayT, n = 1)[,delayTDist]
 
     #Make the simplifications
     sampled_lambdae <- sampled_HRStar*sampled_lambdac
     sampled_gammae <- sampled_gammac
 
-    if (rec_method=="Power"){
-      dataCombined <- SimDTEDataSetPower(n_C, n_E, sampled_gammae, sampled_gammac, sampled_lambdae,
-                                         sampled_lambdac, sampled_delayT, rec_period, rec_power)
-    }
-
-    if (rec_method=="PWC"){
-      dataCombined <- SimDTEDataSetPWC(n_C, n_E, sampled_gammae, sampled_gammac, sampled_lambdae,
-                                       sampled_lambdac, sampled_delayT, rec_rate, rec_duration)
-    }
+    dataCombined <- SimDTEDataSet(n_C, n_E, lambda_C, sampled_HRStar, sampled_gammac, sampled_gammae, sampled_delayT, P_S, P_DTE,
+                              rec_method, rec_period, rec_power, rec_rate, rec_duration)
 
 
-    dataCombined <-
-      CensFunc <- function(dataCombined, censTime){
+    dataCombined <- CensFunc(dataCombined, censEvents, censTime)
 
-
-
-
-
-      }
+    assVec[i] <- survivalAnalysis(dataCombined, analysis_method, alpha = 0.05, rho, gamma)
 
 
   }
 
 }
+
+
