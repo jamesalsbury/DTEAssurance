18bf48e1e27e66239e671c9c39c4542cb747938a
James Salsbury <jsalsbury1@sheffield.ac.uk>
2025-02-05 16:29:40 +0000

    adding to the package

--- R/main.R
+++ R/main.R
@@ -79,94 +79,97 @@ cens_data <- function(data, cens_events = NULL, cens_time = NULL){
   return(list(data = data, cens_events = cens_events, cens_time = cens_time))
 }
 
-delay_time_SHELF <- SHELF::fitdist(c(3, 4, 5), probs = c(0.25, 0.5, 0.75), lower = 0, upper = 10)
-post_delay_HR_SHELF <- SHELF::fitdist(c(0.55, 0.6, 0.7), probs = c(0.25, 0.5, 0.75), lower = 0, upper = 1.5)
-delay_time_dist <- "hist"
-post_delay_HR_dist <- "hist"
-P_S <- 0.8
-P_DTE <- 0.7
-
+#' Calculate Assurance for a trial with a Delayed Treatment Effect
+#'
+#' @param n_c number of patients in the control group
+#' @param n_t number of patients in the control group
+#' @param lambda_c control hazard
+#' @param control_dist distribution of control distribution, must be one of "exponential" (default) or "weibull"
+#' @param delay_time_SHELF A SHELF object, beliefs about the delay time
+#' @param delay_time_dist Distribution of the delay time, "hist" is default. See SHELF help for more details
+#' @param post_delay_HR_SHELF A SHELF object, beliefs about the post-delay hazard ratio
+#' @param post_delay_HR_dist Distribution of the post-delay hazard ratio, "hist" is default. See SHELF help for more details
+#' @param P_S Probability of the survival curves separating
+#' @param P_DTE Probability of the survival curves being subject to a DTE, given they separate
+#' @param cens_events Number of events at which you wish to perform the censoring (must be less than n_c + n_t)
+#' @param cens_time Time at which you wish to perform the censoring
+#' @param rec_method Recruitment method, must be one of "power" or "PWC" (piecewise constant)
+#' @param rec_period Parameter used to model recruitment according to power model
+#' @param rec_power Parameter used to model recruitment according to power model
+#' @param rec_rate Parameter used to model recruitment according to piecewise constant model
+#' @param rec_duration Parameter used to model recruitment according to piecewise constant model
+#' @param analysis_method Method of analysis, "LRT" (default) for standard log-rank test, or "WLRT" for weighted log-rank test
+#' @param alpha Type I error
+#' @param alternative String specifying the alternative hypothesis, must be one of "one.sided" or "two-sided" (default)
+#' @param rho Rho parameter for the Fleming-Harrington weighted log-rank test
+#' @param gamma Gamma parameter for the Fleming-Harrington weighted log-rank test
+#' @param nSims Number of simulations, default is 1000
+#'
+#' @return A list: Assurance value, 95% CI for assurance, Duration
+#' @export
+#'
 
 calc_dte_assurance <- function(n_c, n_t, lambda_c, control_dist = "Exponential",
                                delay_time_SHELF, delay_time_dist = "hist",
                                post_delay_HR_SHELF, post_delay_HR_dist = "hist",
                                P_S = 1, P_DTE = 0,
-                               censEvents = NULL, censTime = NULL,
+                               cens_events = NULL, cens_time = NULL,
                                rec_method, rec_period=NULL, rec_power=NULL, rec_rate=NULL, rec_duration=NULL,
-                               analysis_method, rho = 0, gamma = 0, nSims=1e4){
+                               analysis_method = "LRT", alpha = 0.05, alternative = "one.sided", rho = 0, gamma = 0, nSims=1e3){
 
 
-  if (runif(1) > P_S){
-    #Curves do not separate
-    delay_time <- 0
-    post_delay_HR <- 1
-  } else {
-    if (runif(1) > P_DTE){
-      #Curves separate with no delay
-      delay_time <- 0
-      post_delay_HR_sample <- SHELF::sampleFit(post_delay_HR_SHELF, n = 1)
-      post_delay_HR <- post_delay_HR_sample[,post_delay_HR_dist]
-    } else{
-      #Curves separate with a delay
-      delay_time_sample <- SHELF::sampleFit(delay_time_SHELF(), n = 1)
-      post_delay_HR_sample <- SHELF::sampleFit(post_delay_HR_SHELF(), n = 1)
-      delay_time <- delay_time_sample[,delay_time_dist]
-      post_delay_HR <- post_delay_HR_sample[,post_delay_HR_dist]
-    }
-  }
-
-
-  data <- sim_dte(n_c, n_t, lambda_c, delay_time, post_delay_HR, dist = "exponential", gamma_c = NULL)
-
-  if (rec_method=="power"){
-    data <- add_recruitment_time(data, rec_method,
-                                     rec_period=NULL, rec_power=NULL)
-  }
-
-  if (rec_method == "PWC"){
-    data <- add_recruitment_time(data, rec_method,
-                                      rec_rate=NULL, rec_duration=NULL)
-  }
-
-
-  data <- cens_data(data, cens_events, cens_time)$data
-
-
-
-
-
-
-
+  assurance_vec <- rep(NA, nSims)
+  cens_vec <- rep(NA, nSims)
 
   for (i in 1:nSims){
 
-    #Sample the control parameters
-    u <- sample(1:control_n, size = 1)
-    sampled_lambdac <- lambda_C[u]
-    sampled_gammac <- gamma_C[u]
+    if (runif(1) > P_S){
+      #Curves do not separate
+      delay_time <- 0
+      post_delay_HR <- 1
+    } else {
+      if (runif(1) > P_DTE){
+        #Curves separate with no delay
+        delay_time <- 0
+        post_delay_HR_sample <- SHELF::sampleFit(post_delay_HR_SHELF, n = 1)
+        post_delay_HR <- post_delay_HR_sample[,post_delay_HR_dist]
+      } else{
+        #Curves separate with a delay
+        delay_time_sample <- SHELF::sampleFit(delay_time_SHELF, n = 1)
+        post_delay_HR_sample <- SHELF::sampleFit(post_delay_HR_SHELF, n = 1)
+        delay_time <- delay_time_sample[,delay_time_dist]
+        post_delay_HR <- post_delay_HR_sample[,post_delay_HR_dist]
+      }
+    }
 
-    #Sample the treatment effect parameters
-    sampled_HRStar <- SHELF::sampleFit(HRStar, n = 1)[,HRStarDist]
-    sampled_delayT <- SHELF::sampleFit(delayT, n = 1)[,delayTDist]
 
-    #Make the simplifications
-    sampled_gammae <- sampled_gammac
+    data <- sim_dte(n_c, n_t, lambda_c, delay_time, post_delay_HR, dist = "exponential", gamma_c = NULL)
 
-    censoredDF <- CensFunc(dataCombined, censEvents, censTime)
+    if (rec_method=="power"){
+      data <- add_recruitment_time(data, rec_method,
+                                   rec_period, rec_power)
+    }
 
-    dataCombined <- censoredDF$dataCombined
+    if (rec_method == "PWC"){
+      data <- add_recruitment_time(data, rec_method,
+                                   rec_rate, rec_duration)
+    }
 
-    censVec[i] <- censoredDF$censTime
 
-    assVec[i] <- survivalAnalysis(dataCombined, analysis_method, alpha = 0.05, rho, gamma)
+    data_after_cens <- cens_data(data, cens_events, cens_time)
+    data <- data_after_cens$data
+
+    cens_vec[i] <- data_after_cens$cens_time
+
+    assurance_vec[i] <- survival_test(data, analysis_method, alternative, alpha = 0.05, rho, gamma)
 
 
   }
 
-  pHat <- mean(assVec)
+  assurance <- mean(assurance_vec)
 
-  return(list(assurance = pHat, duration = mean(censVec), LBAssurance = pHat - 1.96*sqrt(pHat*(1-pHat)/nSims),
-              UBAssurance = pHat + 1.96*sqrt(pHat*(1-pHat)/nSims)))
+  return(list(assurance = assurance, CI_Assurance = c(assurance - 1.96*sqrt(assurance*(1-assurance)/nSims),
+              UBAssurance = assurance + 1.96*sqrt(assurance*(1-assurance)/nSims)), duration = mean(cens_vec)))
 
 }
 
@@ -291,19 +294,10 @@ add_recruitment_time <- function(data, rec_method,
     data$rec_time <- rec
   }
 
-
   data$pseudo_time <- data$time + data$rec_time
 
   return(data)
 
 }
-
-
-
-
-
-
-
-
 
 
